d0ffcf22b7ebe78abf3048793c489055bacd9f76
Pavel Samoilov <pavel.samoilov@simbirsoft.com>
2022-06-23 11:47:35 +0400

    Сделали сетевой запрос

--- wheater/src/App.js
+++ wheater/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
+const lat = '54.32824'
+const lon = '48.38657'
+
+const API_key = '4ab6736c562960e2e0217040501cded2'
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric&lang=ru`
+
+function App(props) {
+  const [cloudDesription, setcloudDescription] = useState('Пусто')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url)
+
+      if (response.ok) {
+        const json = await response.json()
+        setcloudDescription(json.weather[0].description)
+      } else {
+        console.error('Что-то не так')
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello world</h1>
+      {cloudDesription}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
